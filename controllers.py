5a3782f6d9601ff3291d7969ed918a0a183b1260
Daniel Matuschek <daniel@hifiberry.com>
2018-02-25 20:42:15 +0100

    First checkin of playback controller code The only controller that is implemented at this time is the Nuimo controller.

---
+++ controllers.py
@@ -0,0 +1,103 @@
+import threading
+import logging
+from typing import Dict
+import nuimohelpers
+
+CONTROL_VOLUME_UP = 1
+CONTROL_VOLUME_DOWN = 2
+CONTROL_NEXT = 4
+CONTROL_PREVIOUS = 8
+
+
+class Controller():
+
+    def __init__(self, params: Dict[str, str]):
+        self.manager = None
+
+    def set_manager(self, manager):
+        self.manager = manager
+
+    def terminate(self):
+        pass
+
+
+class Nuimo(Controller):
+
+    import nuimo
+    import nuimohelpers
+    from nuimo import Gesture, LedMatrix
+
+    class NuimoControllerListener(nuimo.ControllerListener):
+
+        is_app_disconnection = False
+        connection_failed = False
+
+        def started_connecting(self):
+            logging.info("Connecting to Nuimo controller ...")
+
+        def connect_succeeded(self):
+            self.connection_failed = False
+            logging.info("Connected to Nuimo controller")
+            print("SUCCEED")
+            self.controller.display_matrix(nuimohelpers.MUSIC_NOTE)
+
+        def connect_failed(self, error):
+            self.connection_failed = True
+            logging.info("Can't reconnect. Restarting nuimo_app")
+
+        def disconnect_succeeded(self):
+            logging.warn("Disconnected, reconnecting...")
+            if not self.is_app_disconnection:
+                self.controller.connect()
+
+        def services_resolved(self):
+            logging.info("Received services resolved to Nuimo controller")
+
+        def received_gesture_event(self, event):
+            print(event)
+            logging.debug("Received gesture event to Nuimo controller")
+            if event.gesture == Nuimo.Gesture.ROTATION:
+                value = event.value / 15
+                target_vol = self.manager.volume_percent + value
+                self.manager.set_volume_percent(target_vol)
+            elif (event.gesture == Nuimo.Gesture.TOUCH_RIGHT) or \
+                    (event.gesture == Nuimo.Gesture.SWIPE_RIGHT):
+                self.manager.skip(1)
+            elif (event.gesture == Nuimo.Gesture.TOUCH_LEFT) or \
+                    (event.gesture == Nuimo.Gesture.SWIPE_LEFT):
+                self.manager.skip(-1)
+
+        def set_manager(self, manager):
+            self.manager = manager
+
+    def __init__(self, params: Dict[str, str]):
+        import nuimo
+
+        mac = params["mac"]
+
+        self.manager = nuimo.ControllerManager(adapter_name='hci0')
+        self.nuimocontroller = nuimo.Controller(manager=self.manager,
+                                                mac_address=mac)
+        self.nuimocontroller.listener = Nuimo.NuimoControllerListener()
+        self.nuimocontroller.listener.controller = self.nuimocontroller
+        self.nuimocontroller.connect()
+
+        thread = threading.Thread(target=self.runloop, args=())
+        thread.daemon = True
+        thread.start()
+
+    def runloop(self):
+        try:
+            self.manager.run()
+        except KeyboardInterrupt:
+            self.manager.quit()
+
+    def terminate(self):
+        # Terminate GATT event loop
+        self.nuimocontroller.listener.stop()
+        self.nuimocontroller.disconnect()
+        self.manager.stop()
+
+    def set_manager(self, manager):
+        Controller.set_manager(self, manager)
+        self.nuimocontroller.listener.set_manager(manager)
---
+++ nuimohelpers.py
@@ -0,0 +1,198 @@
+# Nuimo matrices from
+# https://github.com/getsenic/senic-hub/blob/master/senic_hub/nuimo_app/matrices.py
+from math import ceil
+
+
+LIGHT_OFF = \
+    "         " \
+    "         " \
+    "         " \
+    "    *    " \
+    "   ***   " \
+    "    *    " \
+    "         " \
+    "         " \
+    "         "
+
+LIGHT_ON = \
+    "    *    " \
+    " *     * " \
+    "         " \
+    "    *    " \
+    "*  ***  *" \
+    "    *    " \
+    "         " \
+    " *     * " \
+    "    *    "
+
+LIGHT_BULB = \
+    "   ***   "  \
+    "  *   *  "  \
+    "  *   *  "  \
+    "  * * *  "  \
+    "  * * *  "  \
+    "  * * *  "  \
+    "   ***   "  \
+    "   ***   "  \
+    "         "
+
+PAUSE = \
+    "         " \
+    "  ## ##  " \
+    "  ## ##  " \
+    "  ## ##  " \
+    "  ## ##  " \
+    "  ## ##  " \
+    "  ## ##  " \
+    "  ## ##  " \
+    "         "
+
+PLAY = \
+    "         " \
+    "   #     " \
+    "   ##    " \
+    "   ###   " \
+    "   ####  " \
+    "   ###   " \
+    "   ##    " \
+    "   #     " \
+    "         "
+
+MUSIC_NOTE = \
+    "  #####  " \
+    "  #####  " \
+    "  #   #  " \
+    "  #   #  " \
+    "  #   #  " \
+    " ##  ##  " \
+    "### ###  " \
+    " #   #   " \
+    "         "
+
+SHUFFLE = \
+    "         " \
+    "         " \
+    " ##   ## " \
+    "   # #   " \
+    "    #    " \
+    "   # #   " \
+    " ##   ## " \
+    "         " \
+    "         "
+
+ERROR = \
+    "         " \
+    "         " \
+    "  *   *  " \
+    "   * *   " \
+    "    *    " \
+    "   * *   " \
+    "  *   *  " \
+    "         " \
+    "         "
+
+NEXT_SONG = \
+    "         " \
+    "         " \
+    "   #  #  " \
+    "   ## #  " \
+    "   ####  " \
+    "   ## #  " \
+    "   #  #  " \
+    "         " \
+    "         "
+
+PREVIOUS_SONG = \
+    "         " \
+    "         " \
+    "  #  #   " \
+    "  # ##   " \
+    "  ####   " \
+    "  # ##   " \
+    "  #  #   " \
+    "         " \
+    "         "
+
+POWER_OFF = \
+    "         " \
+    "         " \
+    "   ###   " \
+    "  #   #  " \
+    "  #   #  " \
+    "  #   #  " \
+    "   ###   " \
+    "         " \
+    "         "
+
+
+LETTER_W = \
+    "         " \
+    " #     # " \
+    " #     # " \
+    " #     # " \
+    " #     # " \
+    " #  #  # " \
+    " #  #  # " \
+    "  ## ##  " \
+    "         "
+
+STATION1 = \
+    "         " \
+    "     *   " \
+    "    **   " \
+    "   * *   " \
+    "     *   " \
+    "     *   " \
+    "     *   " \
+    "     *   " \
+    "         "
+
+STATION2 = \
+    "         " \
+    "   ***   " \
+    "      *  " \
+    "      *  " \
+    "   ***   " \
+    "  *      " \
+    "  *      " \
+    "   ***   " \
+    "         "
+
+STATION3 = \
+    "         " \
+    "   ***   " \
+    "      *  " \
+    "      *  " \
+    "    **   " \
+    "      *  " \
+    "      *  " \
+    "   ***   " \
+    "         "
+
+
+def progress_bar(progress):
+    """
+    Generates a light bar matrix to display volume / brightness level.
+    :param progress: value between 0..1
+    """
+    dots = list(" " * 81)
+    num_dots = ceil(round(progress, 3) * 9)
+    while num_dots > 0:
+        dots[81 - ((num_dots - 1) * 9 + 5)] = "*"
+        num_dots -= 1
+
+    return "".join(dots)
+
+
+def matrix_with_index(matrix, index):
+    """
+    Adds index of the component in the component list to the top right
+    corner of the component matrix.
+    """
+    dots = list(matrix)
+
+    while index >= 0:
+        dots[index * 9 + 8] = '*'
+        index -= 1
+
+    return "".join(dots)
