71285f5cf8e364d85d843ecd9690fcbefa8bba07
HiFiBerry <info@hifiberry.com>
2018-03-03 12:40:26 +0100

    p compatible setup.py

---
+++ setup.py
@@ -0,0 +1,59 @@
+""" Setup module for AudioController """
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='audiocontrol',
+    version='0.0.1',
+    description='A program that manages different audio players',
+    #   long_description=long_description,
+    url='https://github.com/hifiberry/audiocontrol',
+    author='HiFiBerry',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: No Input/Output (Daemon),'
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX'
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3',
+
+    install_requires=[
+        'pyalsaaudio',
+        'keyboard',
+        'nuimo',
+        'spotipy',
+        'zeroconf',
+        'bottle',
+        'xmltodict',
+    ],
+
+    keywords='audio control shairport spotifyd metadata',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    package_data={
+        'config-example': ['configuration-example.txt'],
+    },
+
+    project_urls={
+        'Bug Reports': 'https://github.com/hifiberry/audiocontrol/issues',
+    },
+)
