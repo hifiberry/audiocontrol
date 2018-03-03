e9aa0127c8c1f5dc00005c3655a9b308ddef4897
HiFiBerry <info@hifiberry.com>
2018-03-03 17:39:49 +0100

    Cleanup

--- audiocontrol/__main__.py
+++ audiocontrol/__main__.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        u'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     root.addHandler(ch)
 
