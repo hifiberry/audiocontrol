01b3dacd23b8fe7c6c52aee4a47746b0834bf4c9
HiFiBerry <info@hifiberry.com>
2018-03-03 17:40:29 +0100

    Status FIFO will always generated

--- audiocontrol/audiocontrol.py
+++ audiocontrol/audiocontrol.py
@@ -6,8 +6,6 @@ import sys
 import configparser
 import logging
 
-from typing import Dict
-
 from audiocontrol import Manager
 
 from audiocontrol.backends.spotify import Spotifyd
@@ -29,8 +27,8 @@ class StatusUpdater(object):
         self.status = status
         thread = threading.Thread(target=self.run, args=())
         try:
-            if (not(os.path.exists(statuspipe))):
-                os.mkfifo(statuspipe)
+            os.remove(self.statuspipe)
+            os.mkfifo(statuspipe)
             os.chmod(statuspipe, 0o666)
         except PermissionError:
             logging.error("Can't create named pipe %s", statuspipe)
@@ -56,6 +54,8 @@ class StatusUpdater(object):
         if line == "":
             return
 
+        logging.debug("Received message from FIFO: %s", line.strip())
+
         words = line.split()
 
         if len(words) == 2:
@@ -65,48 +65,55 @@ class StatusUpdater(object):
                 if (self.status.service == words[1]):
                     self.status.service_changed("")
 
+        elif len(words) == 3:
+            if (words[0] == "TRACK"):
+                self.status.track_changed(words[1], words[2])
+
 
 class status():
 
-    def __init__(self, controller, statuspipe="/tmp/audiostatus"):
-        self.controller = controller
+    def __init__(self, manager, statuspipe="/tmp/audiostatus"):
+        self.manager = manager
         self.service = "UNKNOWN"
         self.statusupdater = StatusUpdater(self, statuspipe)
 
     def service_changed(self, service):
         self.service = service
-        self.controller.service_changed(service)
+        self.manager.service_changed(service)
+
+    def track_changed(self, service, trackdata):
+        self.manager.track_changed(service, trackdata)
 
     def received_metadata(self, key, value):
-        print(key, value)
+        logging.debug("Received metadata %s:%s", key, value)
 
 
 class MyManager(Manager):
 
-    def __init__(self):
+    def __init__(self, params):
         super(MyManager, self).__init__()
 
         self.status = status(self)
-        self.stop_other_on_service_change = True
         self.backends = []
         self.controllers = []
         self.terminate = False
         self.volume_percent = 50
         self.active_backend = None
 
+        self.stop_other_on_service_change = params.get("stop_other_on_service_change",
+                                                       True)
+
     def run(self):
-        print("Backends enabled: ")
-        for b in self.backends:
-            print(b)
-        print("Controllers enabled: ")
-        for c in self.controllers:
-            print(c)
+        logging.info("Backends enabled: %s",
+                     ",".join(str(b) for b in self.backends))
+        logging.info("Controllers enabled: %s",
+                     ",".join(str(c) for c in self.controllers))
 
         while not(self.terminate):
             time.sleep(0.1)
 
     def service_changed(self, service):
-        print("service: {}".format(service))
+        logging.debug("service: %s", service)
         if self.stop_other_on_service_change and (service != ""):
             for b in self.backends:
                 if b.service == service:
@@ -116,11 +123,17 @@ class MyManager(Manager):
                     b.stop()
                     b.activate(False)
 
+    def track_changed(self, service, trackdata):
+        logging.debug("Track changed %s:%s", service, trackdata)
+        for b in self.backends:
+            if b.service == service:
+                b.track_changed(trackdata)
+
     def quit(self):
         self.terminate = True
 
     def skip(self, direction=1):
-        print("Skip {}".format(direction))
+        logging.debug("Skip %s", direction)
         if self.active_backend is not None:
             self.active_backend.skip(direction)
 
@@ -150,26 +163,31 @@ def sigusr1_handler(_signal, _frame):
 def run():
     global manager
 
-    manager = MyManager()
-
     config = configparser.ConfigParser()
-    config.read("audiocontrol.conf")
+    config.read("/etc/audiocontrol.conf")
+
+    if "general" in config.sections():
+        manager = MyManager(config["general"])
+    else:
+        manager = MyManager({})
 
     for section in config.sections():
         if section == "spotify":
-            manager.add_backend(Spotifyd({}))
+            manager.add_backend(Spotifyd(config["spotify"]))
 
         if section == "shairport":
-            manager.add_backend(Shairport({}))
+            manager.add_backend(Shairport(config["shairport"]))
 
         if section == "mpd":
             manager.add_backend(Mpd({}))
 
+        if section == "btspaker":
+            manager.add_backend(BTSpeaker(config["btspeaker"]))
+
+        # ----
+
         if section == "nuimo":
             manager.add_controller(Nuimo(config["nuimo"]))
-
-        if section == "btspaker":
-            manager.add_controller(BTSpeaker(config["btspeaker"]))
 
         if section == "keyboard":
             manager.add_controller(Keyboard(config["keyboard"]))
